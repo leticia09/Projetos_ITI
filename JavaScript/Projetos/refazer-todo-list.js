f50acd87c5aedd6693a4711e54eef3fe1e678704
leticia <lelealvesaz@gmail.com>
2020-09-08 00:05:02 -0300

    Finalizando projeto

--- JavaScript/Projetos/refazer-todo-list.js
+++ JavaScript/Projetos/refazer-todo-list.js
@@ -4,9 +4,11 @@ const filterOption = document.querySelector("#filter-todo");
 const todoList = document.querySelector(".todo-list");
 
 todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+filterOption.addEventListener('click', filterTodo);
 
 
-function addTodo(event){
+function addTodo(event) {
     event.preventDefault();
     const todoDiv = document.createElement("div");
     todoDiv.classList.add("todo");
@@ -31,4 +33,47 @@ function addTodo(event){
     // colocar toda lista aparente
     todoList.appendChild(todoDiv);
     todoInput.value = " ";
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    if (item.classList[0] === 'trash-btn') {
+        //não entendi essa parte
+        const todo = item.parentElement;
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function () {
+            todo.remove();
+        });
+    }
+    if (item.classList[0] === 'completed-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
+    }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function (todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = 'flex';
+                break;
+            case "completed":
+                //porque ele usa o contains?
+                if (todo.classList.contains("completed")) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+            case "uncompleted":
+                // porque se no if eu colocar o uncompleted não funciona?
+                if (!todo.classList.contains("completed")) {
+                    todo.style.display = 'flex';
+                } else {
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    })
 }
